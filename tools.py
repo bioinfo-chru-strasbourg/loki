945c173eae2819b81317c64944e42fb9cbf99777
bioinfo <bioinfo.chru.strasbourg@gmail.com>
2023-10-20 17:50:41 +0200

    Fixed hap.py not working with FT filter and others app than SOMATIC

---
+++ tools.py
@@ -0,0 +1,4 @@
+bcftools = "/home1/HUB/bin/bcftools/1.14/bin/bcftools"
+tabix = "/home1/data/env/loki_env/bin/tabix"
+bgzip = "/home1/data/env/loki_env/bin/bgzip"
+bedtools = "/home1/data/env/loki_env/bin/bedtools"
